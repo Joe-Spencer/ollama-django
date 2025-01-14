bf6ec94b440e797cb0cacc3eb7b00a72f5571c6d
Joe <31321326+Joe-Spencer@users.noreply.github.com>
2025-01-14 14:30:32 -0500

    Improved Aesthetics

--- chat/ollama_api.py
+++ chat/ollama_api.py
@@ -1,6 +1,10 @@
 import ollama
 
 def generate_response(messages):
+    model='llama3'
+    LastMessage = messages[-1]
+    if 'llava' in LastMessage['content'].lower():
+        model='llava'
     response = ollama.chat(
         model='llama3',
         messages=messages,
